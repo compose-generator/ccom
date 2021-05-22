ad333788d52d05972338fa53b6961feac948b59a
Splines <dominic-plein@gmx.de>
2021-05-22 04:14:51 +0200

    Init FileReaderTest

---
+++ compiler/java/src/test/java/com/c/FileReaderTest.java
@@ -0,0 +1,16 @@
+package com.c;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FileReaderTest {
+
+    @Test
+    @DisplayName("TODO")
+    public void testTodo() {
+        // TODO
+    }
+
+}
