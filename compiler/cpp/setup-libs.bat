7ad77f88eb7562d906df3096186a1718034c3fa2
Marc Auberer <marc.auberer@chillibits.com>
2021-05-27 00:56:43 +0200

    Adjust CI workflow

--- compiler/cpp/setup-libs.bat
+++ compiler/cpp/setup-libs.bat
@@ -1,4 +1,4 @@
 @echo off
 cd lib
 git clone https://github.com/nlohmann/json.git
-curl "https://github.com/nlohmann/json/releases/download/v3.9.1/json.hpp" --output json/json.hpp
+curl "https://github.com/nlohmann/json/releases/download/v3.9.1/json.hpp" --output lib/json/json.hpp
--- compiler/cpp/test/ReaderTest.cpp
+++ compiler/cpp/test/ReaderTest.cpp
@@ -2,8 +2,37 @@
 // Created by Marc on 27.05.2021.
 //
 
+#include <reader/Reader.h>
 #include "gtest/gtest.h"
 
 TEST(ReaderTests, MaxLookahead) {
-    EXPECT_EQ (1,  1);
+    Reader reader = Reader("File input", 4);
+    EXPECT_EQ (reader.getMaxLookahead(),  "File");
+}
+
+TEST(ReaderTests, Lookahead) {
+    Reader reader = Reader("File input", 2);
+    EXPECT_EQ (reader.getLookahead(),  'F');
+}
+
+TEST(ReaderTests, AdvanceLookahead) {
+    Reader reader = Reader("File input", 2);
+    EXPECT_EQ (reader.getLookahead(),  'F');
+    reader.advance();
+    EXPECT_EQ (reader.getLookahead(),  'i');
+    reader.advance();
+    EXPECT_EQ (reader.getLookahead(),  'l');
+    reader.advance();
+    EXPECT_EQ (reader.getLookahead(),  'e');
+}
+
+TEST(ReaderTests, AdvanceMexLookahead) {
+    Reader reader = Reader("File input", 3);
+    EXPECT_EQ (reader.getMaxLookahead(),  "Fil");
+    reader.advance();
+    EXPECT_EQ (reader.getMaxLookahead(),  "ile");
+    reader.advance();
+    EXPECT_EQ (reader.getMaxLookahead(),  "le ");
+    reader.advance();
+    EXPECT_EQ (reader.getMaxLookahead(),  "e i");
 }
