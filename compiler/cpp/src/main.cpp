068608d5031accd5177350f1aae57b3853e47901
Marc Auberer <marc.auberer@chillibits.com>
2021-05-27 12:07:41 +0200

    Re-add outcommented code

--- compiler/cpp/src/main.cpp
+++ compiler/cpp/src/main.cpp
@@ -9,25 +9,25 @@
 // CLI call: ./ccom <single-stmt> <file-input> <data-input> <com-line-iden> <com-block-open-iden> <com-block-close-iden>
 int main(int argc, char** argv) {
     // Parse cli args
-    /*std::vector<std::string> args;
+    std::vector<std::string> args;
     for (size_t iArg = 0; iArg < argc; ++iArg)
-        args.emplace_back(argv[iArg]);*/
+        args.emplace_back(argv[iArg]);
 
-    bool singleStatementMode = false; //args[1] == "true";
-    std::string fileInput = "File input"; //args[2];
-    std::string dataInput = "{}"; //args[3];
-    std::string comLineIden = "//"; //args[4];
-    std::string comBlockOpenIden = "/*"; //args[5];
-    std::string comBlockCloseIden = "*/"; //args[6];
+    bool singleStatementMode = args[1] == "true";
+    std::string fileInput = args[2];
+    std::string dataInput = args[3];
+    std::string comLineIden = args[4];
+    std::string comBlockOpenIden = args[5];
+    std::string comBlockCloseIden = args[6];
 
     // Replace file input with test string
     //fileInput = "property1:= value\n//? if has frontend | test.Test == 90133 | var.FlaskPort == \"8\\\"080\\\"\" {\n// test payload}\n// - another test payload\n//? }\nattribute2: value2";
     //fileInput = "property1:= value\n/*? if has frontend | test.Test == 90133 | var.FlaskPort == \"8\\\"080\\\"\" {\ntest payload\n- }another test payload\n}*/\nattribute2: value2";
     //fileInput = "//? if not has test.marc | test.marc.dominic.24 != \"Test\" { Test payload //? }";
 
-    comLineIden = "";
-    comBlockOpenIden = "<!--";
-    comBlockCloseIden = "-->";
+    //comLineIden = "";
+    //comBlockOpenIden = "<!--";
+    //comBlockCloseIden = "-->";
 
     // Start compiler pipeline
     Interpreter interpreter = Interpreter(singleStatementMode, fileInput, dataInput,
