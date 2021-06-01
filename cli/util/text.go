3627428e3d42a32b623c57afc288f3b4d4bf6ca6
Marc Auberer <marc.auberer@chillibits.com>
2021-06-01 21:46:37 +0200

    Add tests for text util

--- cli/util/text.go
+++ cli/util/text.go
@@ -2,6 +2,7 @@ package util
 
 import "encoding/json"
 
+// IsJson checks if the input string is a valid json structure
 func IsJson(s string) bool {
 	var js map[string]interface{}
 	return json.Unmarshal([]byte(s), &js) == nil
