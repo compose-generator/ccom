a1676886db10f843389f84f4a30f825fd9468ea4
Marc Auberer <marc.auberer@chillibits.com>
2021-06-01 22:46:10 +0200

    Add tests for preprocessor

---
+++ cli/preprocessor_test.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// ------------------------------------- getCommentIdenFromLang -------------------------------------
+
+func TestGetComIdenFromLang_Success1(t *testing.T) {
+	// Manual input
+	langList := []string{"yaml", "yml", "python", "py", "docker", "dockerfile"}
+	for _, lang := range langList {
+		lineComIden, blockComIdenOpen, blockComIdenClose := getCommentIdenFromLang(lang, "")
+		assert.Equal(t, "#", lineComIden)
+		assert.Empty(t, blockComIdenOpen)
+		assert.Empty(t, blockComIdenClose)
+	}
+	// Auto
+	langList = []string{"yaml", "yml", "py"}
+	for _, lang := range langList {
+		lineComIden, blockComIdenOpen, blockComIdenClose := getCommentIdenFromLang("auto", "./test-files/test."+lang)
+		assert.Equal(t, "#", lineComIden)
+		assert.Empty(t, blockComIdenOpen)
+		assert.Empty(t, blockComIdenClose)
+	}
+}
+
+func TestGetComIdenFromLang_Success2(t *testing.T) {
+	// Manual
+	langList := []string{"java", "c", "c++", "cpp", "golang", "go", "javascript", "js", "typescript", "ts", "rust", "rs"}
+	for _, lang := range langList {
+		lineComIden, blockComIdenOpen, blockComIdenClose := getCommentIdenFromLang(lang, "")
+		assert.Equal(t, "//", lineComIden)
+		assert.Equal(t, "/*", blockComIdenOpen)
+		assert.Equal(t, "*/", blockComIdenClose)
+	}
+	// Auto
+	langList = []string{"java", "c", "cpp", "go", "js", "ts", "rs"}
+	for _, lang := range langList {
+		lineComIden, blockComIdenOpen, blockComIdenClose := getCommentIdenFromLang("auto", "./test-files/test."+lang)
+		assert.Equal(t, "//", lineComIden)
+		assert.Equal(t, "/*", blockComIdenOpen)
+		assert.Equal(t, "*/", blockComIdenClose)
+	}
+}
+
+func TestGetComIdenFromLang_Success3(t *testing.T) {
+	langList := []string{"html", "htm", "xml"}
+	// Manual
+	for _, lang := range langList {
+		lineComIden, blockComIdenOpen, blockComIdenClose := getCommentIdenFromLang(lang, "")
+		assert.Empty(t, lineComIden)
+		assert.Equal(t, "<!--", blockComIdenOpen)
+		assert.Equal(t, "-->", blockComIdenClose)
+	}
+	// Auto
+	for _, lang := range langList {
+		lineComIden, blockComIdenOpen, blockComIdenClose := getCommentIdenFromLang("auto", "./test-files/test."+lang)
+		assert.Empty(t, lineComIden)
+		assert.Equal(t, "<!--", blockComIdenOpen)
+		assert.Equal(t, "-->", blockComIdenClose)
+	}
+}
+
+// -------------------------------------- ensureFileInputString -------------------------------------
+
+func TestEnsureFileInputString_File(t *testing.T) {
+	testInput := "./test-files/test.txt"
+	ensureFileInputString(&testInput)
+	assert.Equal(t, "This is a test content", testInput)
+}
+
+func TestEnsureFileInputString_String(t *testing.T) {
+	testInput := "Test input"
+	ensureFileInputString(&testInput)
+	assert.Equal(t, "Test input", testInput)
+}
+
+// -------------------------------------- ensureDataInputString -------------------------------------
+
+func TestEnsureDataInputString_File(t *testing.T) {
+	testData := "./test-files/test-data.json"
+	ensureDataInputString(&testData)
+	assert.Equal(t, "{\"attribute1\": true, \"attribute2\": \"test\"}", testData)
+}
+
+func TestEnsureDataInputString_String(t *testing.T) {
+	testData := "{ \"attribute1\": true,\n\"attribute2\": \"test\" }"
+	ensureDataInputString(&testData)
+	assert.Equal(t, "{ \"attribute1\": true,\n\"attribute2\": \"test\" }", testData)
+}
