1fd16699ec83570dfde46cfd9e68d1a7d08fb88f
Marc Auberer <marc.auberer@chillibits.com>
2021-06-18 18:40:44 +0200

    Add GoDoc to text utils

--- cli/util/text.go
+++ cli/util/text.go
@@ -7,26 +7,32 @@ import (
 	"github.com/fatih/color"
 )
 
+// P prints text in white to the console without a line break at the end
 func P(text string) {
 	color.New(color.FgWhite).Print(text)
 }
 
+// Pl prints text in white to the console with a like break at the end
 func Pl(text string) {
 	color.White(text)
 }
 
+// Pel prints an empty line to the console
 func Pel() {
 	fmt.Println()
 }
 
+// Info prints text in yellow to the console
 func Info(text string) {
 	color.Yellow(text)
 }
 
+// Done prints 'done' in green to the console
 func Done() {
 	color.Green("done")
 }
 
+// Error prints an error message in red to the console and exits if the exit flag was set
 func Error(message string, exit bool) {
 	color.Red(message)
 	if exit {
