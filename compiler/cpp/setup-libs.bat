76be0ec4db4df270ba3f6f5352935763762b6262
Marc Auberer <marc.auberer@sap.com>
2022-03-02 16:17:32 +0100

    Update dependencies (#107)

--- compiler/cpp/setup-libs.bat
+++ compiler/cpp/setup-libs.bat
@@ -2,5 +2,5 @@
 mkdir lib
 cd lib
 mkdir json
-curl -SsL "https://github.com/nlohmann/json/releases/download/v3.10.0/json.hpp" --output lib/json/json.hpp
+curl -SsL "https://github.com/nlohmann/json/releases/download/v3.10.5/json.hpp" --output lib/json/json.hpp
 git clone https://github.com/google/googletest.git
